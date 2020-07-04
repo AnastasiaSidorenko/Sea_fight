c220c5b81592d5a3f935fef407af99a74049b6f0
Anastasia <39671563+AnastasiaSidorenko@users.noreply.github.com>
2020-07-04 09:47:36 +0300

    Setting base environment for app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h2>APP</h2>
+            </div>
+        );
+    }
+}
+
+
+export default App;
