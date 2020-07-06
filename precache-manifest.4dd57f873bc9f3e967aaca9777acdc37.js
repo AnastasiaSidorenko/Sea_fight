9dee8dadaa225dc5ff2b520bd0bbc58cfa300f86
Anastasia <39671563+AnastasiaSidorenko@users.noreply.github.com>
2020-07-06 20:44:26 +0300

    Updates

--- precache-manifest.4dd57f873bc9f3e967aaca9777acdc37.js
+++ precache-manifest.4dd57f873bc9f3e967aaca9777acdc37.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1e9ab32cc3eba9ca88c30e1130e3cfb6",
+    "revision": "b18db33d1a874e5f3103d5534b6739e6",
     "url": "/Sea_fight/index.html"
   },
   {
-    "revision": "e4ba271711984122f5b2",
-    "url": "/Sea_fight/static/css/main.ba43c990.chunk.css"
+    "revision": "69432483de77b98bd758",
+    "url": "/Sea_fight/static/css/main.ab0ee5d2.chunk.css"
   },
   {
     "revision": "be6da27cdee9264ca988",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Sea_fight/static/js/2.4c912ada.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e4ba271711984122f5b2",
+    "revision": "69432483de77b98bd758",
     "url": "/Sea_fight/static/js/main.0f87bed0.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Sea_fight/precache-manifest.9a82c96fc074d156cea04fc153a96793.js"
+  "/Sea_fight/precache-manifest.4dd57f873bc9f3e967aaca9777acdc37.js"
 );
 
 self.addEventListener('message', (event) => {
