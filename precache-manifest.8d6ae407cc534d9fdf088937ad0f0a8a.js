8f3e69a963f1b93c1deae3be693548211daf92f7
Anastasia <39671563+AnastasiaSidorenko@users.noreply.github.com>
2020-07-06 19:14:57 +0300

    Updates

---
+++ precache-manifest.8d6ae407cc534d9fdf088937ad0f0a8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d20028f89536c105ff288ed0f48ea05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16e935f1b99cbee76caf",
+    "url": "/static/css/main.7ed76804.chunk.css"
+  },
+  {
+    "revision": "be6da27cdee9264ca988",
+    "url": "/static/js/2.4c912ada.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4c912ada.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16e935f1b99cbee76caf",
+    "url": "/static/js/main.236696e0.chunk.js"
+  },
+  {
+    "revision": "6d0008dc539fadb8740a",
+    "url": "/static/js/runtime-main.6a35b874.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sea_fight/precache-manifest.75e594dbe42dd31cb2c80771638ce405.js"
+  "/precache-manifest.8d6ae407cc534d9fdf088937ad0f0a8a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sea_fight/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
